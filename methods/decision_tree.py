5c7f521fff95114f97c3834a0cab8f8eb5d39d05
nkhaldi <mnk80898@gmail.com>
2020-05-03 02:01:47 +0300

    added knn, kmeans and linear_regression

--- methods/decision_tree.py
+++ methods/decision_tree.py
@@ -6,31 +6,22 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
 from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree._export import plot_tree
+from sklearn.tree import DecisionTreeClassifier
 
-# Parameters
 n_classes = 3
 plot_colors = "ryb"
 plot_step = 0.02
-
-# Load data
 iris = load_iris()
 
 for pairidx, pair in enumerate([[0, 1], [0, 2], [0, 3],
                                 [1, 2], [1, 3], [2, 3]]):
-    # We only take the two corresponding features
     X = iris.data[:, pair]
     y = iris.target
-
-    # Train
     clf = DecisionTreeClassifier().fit(X, y)
 
-    # Plot the decision boundary
     plt.subplot(2, 3, pairidx + 1)
-
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
@@ -44,7 +35,6 @@ for pairidx, pair in enumerate([[0, 1], [0, 2], [0, 3],
     plt.xlabel(iris.feature_names[pair[0]])
     plt.ylabel(iris.feature_names[pair[1]])
 
-    # Plot the training points
     for i, color in zip(range(n_classes), plot_colors):
         idx = np.where(y == i)
         plt.scatter(X[idx, 0], X[idx, 1], c=color, label=iris.target_names[i],
@@ -53,7 +43,6 @@ for pairidx, pair in enumerate([[0, 1], [0, 2], [0, 3],
 plt.suptitle("Decision surface of a decision tree using paired features")
 plt.legend(loc='lower right', borderpad=0, handletextpad=0)
 plt.axis("tight")
-
 plt.figure()
 clf = DecisionTreeClassifier().fit(iris.data, iris.target)
 plot_tree(clf, filled=True)
